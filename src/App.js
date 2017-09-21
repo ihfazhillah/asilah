6398d8f3bb4b3b6c8e116a8519a7ff4bfc0e4dd2
ihfazhillah <mihfazhillah@gmail.com>
2017-09-21 14:16:12 +0700

    retrieve data from graph.cool

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {graphql} from 'react-apollo'
+import gql from 'graphql-tag'
+
+const ItemList = (props) => (
+  <div>
+    <div className="api">
+      <div className="label">{props.title}</div>
+      <div className="amount has-text-centered">{props.content}</div>
+    </div>
+    <hr/>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="section">
         <div className="status-list">
-          <div className="api">
-            <div className="label">API</div>
-            <div className="bar"><progress className="progress is-success" value="99" max="100">99%</progress></div>
-            <div className="amount has-text-centered">99%</div>
-          </div>
-          <hr/>
-          <div className="lib">
-            <div className="label">library.js</div>
-            <div className="bar"><progress className="progress is-success" value="99" max="100">99%</progress></div>
-            <div className="amount has-text-centered">99%</div>
-          </div>
-          <hr/>
-          <div className="website">
-            <div className="label">website</div>
-            <div className="bar"><progress className="progress is-success" value="100" max="100">100%</progress></div>
-            <div className="amount has-text-centered">100%</div>
-          </div>
-          <hr/>
-          <div className="website">
-            <div className="label">something hacky</div>
-            <div className="bar"><progress className="progress is-warning" value="40" max="100">40%</progress></div>
-            <div className="amount has-text-centered">40%</div>
-          </div>
-          <hr/>
-          <div className="website">
-            <div className="label">something unreliable</div>
-            <div className="bar"><progress className="progress is-danger" value="14" max="100">14%</progress></div>
-            <div className="amount has-text-centered">14%</div>
-          </div>
+          {this.props.posts.map(post => (
+            <ItemList
+              title={post.title}
+              content={post.content}
+            />
+          ))}
         </div>
       </div>
     );
   }
 }
+App = graphql(gql`
+query{
+  allPosts{
+    content
+    title
+    createdAt
+    questions{
+      id
+      choices
+      question
+      answer
+    }
+  }
+}
+`, {
+  props: ({ownProps, data}) => {
+    if (!data.loading) {
+      let posts = data.allPosts
+      let postLoading = data.loading
 
+      return {
+        posts,
+        postLoading
+      }
+    } else {
+      return {
+        postLoading: data.loading,
+        posts: []
+      }
+    }
+  }
+})(App)
 export default App;
