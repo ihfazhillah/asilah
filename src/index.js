0923b8c79356805fde8682d088ab89a4f2e22355
ihfazhillah <mihfazhillah@gmail.com>
2017-09-21 19:54:05 +0700

    adding switch

--- src/index.js
+++ src/index.js
@@ -8,16 +8,19 @@ import client from './apollo';
 import {ApolloProvider} from 'react-apollo'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
 const Main = () => (
   <ApolloProvider client={client}>
     <Router>
-      <div>
-        <Route exact path="/" component={Home}/>
-        <Route path="/question/:slug" component={SingleQuestion}/>
-      </div>
+      <Switch>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/question/:slug" component={SingleQuestion}/>
+        </div>
+      </Switch>
     </Router>
   </ApolloProvider>
 )
